6032c8e489cccac3a1bd08a2c0410a45d62fc00a
Enayatullah Norozi <enayathushmand12@gmail.com>
2024-02-17 05:11:32 +0100

    Improve layout a bit, add caps lock somewhere to be able to toggle it somehow.

--- keyboards/omkbd/ergodash/mini/keymaps/enayaaa/keymap.c
+++ keyboards/omkbd/ergodash/mini/keymaps/enayaaa/keymap.c
@@ -22,13 +22,16 @@ enum custom_keycodes {
 //Tap Dance Declarations
 enum {
   TD_LGUI_TOGGLE_QWERTY = 0,
+  TD_ALGR_TOGGLE_CAPS = 1,
 };
 
 tap_dance_action_t tap_dance_actions[] = {
   [TD_LGUI_TOGGLE_QWERTY] = ACTION_TAP_DANCE_LAYER_TOGGLE(KC_LGUI, _QWERTY),
+  [TD_ALGR_TOGGLE_CAPS] = ACTION_TAP_DANCE_DOUBLE(KC_ALGR, KC_CAPS),
 };
 
 #define LG_TQWE TD(TD_LGUI_TOGGLE_QWERTY)
+#define AG_CAPS TD(TD_ALGR_TOGGLE_CAPS)
 
 #define LG(kc) LGUI_T(kc)
 #define LA(kc) LALT_T(kc)
@@ -45,27 +48,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  US_Q,    US_W,    US_F,    US_P,    US_B,    US_MINS,                        US_EQL , US_J,    US_L,    US_U,    US_Y,    US_SCLN, US_BSLS,
     LG(KC_ESC),US_A,  US_R,    US_S,    US_T,    US_G,    US_LBRC,                        US_RBRC, US_M,    US_N,    US_E,    US_I,    US_O,    RG(US_QUOT),
     KC_LSFT, US_Z,    US_X,    US_C,    US_D,    US_V,    XXXXXXX,                        XXXXXXX, US_K,    US_H,    US_COMM, US_DOT,  US_SLSH, KC_RSFT,
-    KC_LCTL, KC_ALGR, LG_TQWE, KC_LALT,          LOWER,   KC_SPC , KC_DEL,       KC_ENT,  KC_BSPC, RAISE,            KC_LEFT, KC_DOWN, KC_UP,   LC(KC_RGHT)
+    KC_LCTL, AG_CAPS, LG_TQWE, KC_LALT,          LOWER,   KC_SPC , KC_DEL,       KC_ENT,  KC_BSPC, RAISE,            KC_LEFT, KC_DOWN, KC_UP,   LC(KC_RGHT)
   ),
 
   [_QWERTY] = LAYOUT(
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_MINS,                        KC_EQL , KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
     LG(KC_ESC),KC_A,  KC_S,    KC_D,    KC_F,    KC_G,    KC_LBRC,                        KC_RBRC, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, RG(KC_QUOT),
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_SPC ,                        KC_BSPC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-    KC_LCTL, KC_ALGR, LG_TQWE, KC_LALT,          LOWER,   KC_SPC , KC_DEL,       KC_ENT,  KC_BSPC, RAISE,            KC_LEFT, KC_DOWN, KC_UP,   LC(KC_RGHT)
+    KC_LCTL, AG_CAPS, LG_TQWE, KC_LALT,          LOWER,   KC_SPC , KC_DEL,       KC_ENT,  KC_BSPC, RAISE,            KC_LEFT, KC_DOWN, KC_UP,   LC(KC_RGHT)
   ),
 
   [_LOWER] = LAYOUT(
     US_TILD, US_1,    US_2,    US_3,    US_4,    US_5,    US_UNDS,                        US_PLUS, US_6,    US_7,    US_8,    US_9,    US_0,    US_ARNG,
     _______, US_CIRC, US_AMPR, US_LPRN, US_RPRN, US_ASTR, US_LCBR,                        KC_RCBR, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, US_ODIA, RG(US_ADIA),
-    _______, US_EXLM, US_AT,   US_HASH, US_DLR,  US_PERC, XXXXXXX,                        XXXXXXX, US_ACUT, US_GRV,  KC_LT,   KC_GT,   US_QUES, _______,
+    _______, US_EXLM, US_AT,   US_HASH, US_DLR,  US_PERC, XXXXXXX,                        XXXXXXX, US_ACUT, US_GRV,  US_SCLN, US_COLN, US_MINS, _______,
     _______, _______, _______, _______,          _______, _______, _______,      _______, _______, _______,          KC_HOME, KC_PGDN, KC_PGUP, LC(KC_END)
   ),
 
   [_RAISE] = LAYOUT(
     US_TILD, US_1,    US_2,    US_3,    US_4,    US_5,    US_UNDS,                        US_PLUS, US_6,    US_7,    US_8,    US_9,    US_0,    US_ARNG,
     _______, US_CIRC, US_AMPR, US_LPRN, US_RPRN, US_ASTR, US_LCBR,                        KC_RCBR, US_H,    US_J,    US_K,    US_L,    US_ODIA, RG(US_ADIA),
-    _______, US_EXLM, US_AT,   US_HASH, US_DLR,  US_PERC, XXXXXXX,                        XXXXXXX, US_ACUT, US_GRV,  KC_LT,   KC_GT,   US_QUES, _______,
+    _______, US_EXLM, US_AT,   US_HASH, US_DLR,  US_PERC, XXXXXXX,                        XXXXXXX, US_ACUT, US_GRV,  US_SCLN, US_COLN, US_MINS, _______,
     _______, _______, _______, _______,          _______, _______, _______,      _______, _______, _______,          KC_HOME, KC_PGDN, KC_PGUP, LC(KC_END)
   ),
 
@@ -129,8 +132,10 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 #define HSV_MY_BLUE      170, 255, 100
 
 const rgblight_segment_t PROGMEM my_capslock_layer[] = RGBLIGHT_LAYER_SEGMENTS(
-    {6, 4, HSV_RED},       // Light 4 LEDs, starting with LED 6
-    {12, 4, HSV_RED}       // Light 4 LEDs, starting with LED 12
+    {0, 3, HSV_MY_RED}, // Light 4 LEDs, starting with LED 6
+    {10, 3, HSV_MY_RED},
+    {7, 1, HSV_MY_RED},
+    {17, 1, HSV_MY_RED}
 );
 const rgblight_segment_t PROGMEM my_cmdh_layer[] = RGBLIGHT_LAYER_SEGMENTS(
     // {0, 20, HSV_GREEN}
